974b7144e146df0de615964d66c3412add78ced9
Adrien Duclos <adrien.duclos@milibris.com>
2015-06-23 19:05:54 +0200

    Basis of dragndrop logic for grid

--- src/Rocketfuel/Display.hs
+++ src/Rocketfuel/Display.hs
@@ -6,14 +6,18 @@ module Rocketfuel.Display (
 
 import Data.Maybe
 
+import Debug.Trace
 import Graphics.Gloss
 import Graphics.Gloss.Juicy
 import Graphics.Gloss.Interface.Pure.Game
 
 import Rocketfuel.Grid
 
+type DragDropState = Maybe Position
+
 data GameContext = GameContext { _resources :: [Picture],
-                                 grid :: Grid }
+                                 grid :: Grid,
+                                 dragDropState :: DragDropState }
 
 resources :: [String]
 resources = ["img/fuel.png",
@@ -44,11 +48,11 @@ loop :: GameContext -> IO ()
 loop game = play displayContext black 60 game displayGrid eventHandling stepHandling
 
 idxListOfList :: [[a]] -> [(Integer, [(Integer, a)])]
-idxListOfList = zip [1..] . map (zip [1..])
+idxListOfList = zip [0..] . map (zip [0..])
 
 displayGrid :: GameContext -> Picture
-displayGrid (GameContext res _grid) 
-    = Pictures . catMaybes . concatMap (pictureLine res) . idxListOfList $ _grid
+displayGrid (GameContext res _grid _) 
+    = translate (-320.0) (120.0) $ Pictures . catMaybes . concatMap (pictureLine res) . idxListOfList $ _grid
 
 pictureLine :: [Picture] -> (Integer, [(Integer, Maybe Cell)]) -> [Maybe Picture]
 pictureLine res (y, xx) = map (uncurry (pictureCell res y)) xx
@@ -59,7 +63,22 @@ pictureCell res y x (Just c) = Just $ translate (fromIntegral y * 32.0)
                                                 (cellToResources res c)
 pictureCell _ _ _ Nothing = Nothing
 
+cellFromCoord :: Float -> Float -> (Integer, Integer)
+cellFromCoord x y = (round (globalX / 32.0), 7 - round (globalY / 32.0))
+    where globalX = x + 320
+          globalY = y - 120
+
+modifyIfNeeded :: Float -> Float -> GameContext -> GameContext
+modifyIfNeeded x y g@(GameContext _ _ Nothing) 
+    = if uncurry legit coords
+            then traceShow coords $ g { dragDropState = Just coords }
+            else g
+    where coords = cellFromCoord x y
+          legit x y = x < 8 && x >= 0 && y < 8 && y >= 0
+modifyIfNeeded _ _ g = g
+
 eventHandling :: Event -> GameContext -> GameContext
+eventHandling (EventKey (MouseButton _) Down _ (x, y)) g = modifyIfNeeded x y g
 eventHandling _ g = g
 
 stepHandling :: Float -> GameContext -> GameContext
