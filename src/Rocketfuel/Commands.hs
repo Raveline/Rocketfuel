7d1eabe938b0b81197d9f58607e5e281fce0526c
Adrien Duclos <adrien.duclos@milibris.com>
2015-08-25 15:19:59 +0200

    Swap command implementation

--- src/Rocketfuel/Commands.hs
+++ src/Rocketfuel/Commands.hs
@@ -5,18 +5,14 @@ module Rocketfuel.Commands (
 import Rocketfuel.Types
 import Rocketfuel.Grid
 
-execute :: GameContext -> GameContext
-execute = removeCommand . execute'
+execute :: Command -> GameContext -> GameContext
+execute (DragAndDrop (Just p1) (Just p2)) gc = swap p1 p2 gc
+execute _ gc = gc
 
-removeCommand :: GameContext -> GameContext
-removeCommand g = g { command = Nothing }
-
-execute' :: GameContext -> GameContext
-execute' gc@(GameContext g (Just (DragAndDrop (Just p1) (Just p2))))
-    = if p1 /= p2 && orthoClose p1 p2 
-        then gc { grid = applySwap (Swap p1 p2) g }
-        else gc
-execute' gc = gc
+swap :: Position -> Position -> GameContext -> GameContext
+swap p1 p2 gc@(GameContext g _) = if p1 /= p2 && orthoClose p1 p2 
+                    then gc { grid = applySwap (Swap p1 p2) g }
+                    else gc
 
 orthoClose :: Position -> Position -> Bool
 orthoClose (x,y) (x2,y2) = abs(x2-x) + abs(y2-y) == 1
